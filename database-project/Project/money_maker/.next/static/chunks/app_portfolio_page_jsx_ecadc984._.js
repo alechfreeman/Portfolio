80abadf3a3484cb6a2539fb91f1a750303c0220d
alechfreeman <ahf210002@utdallas.edu>
2025-11-15 18:09:18 -0600

    Remove Project...

--- database-project/Project/money_maker/.next/static/chunks/app_portfolio_page_jsx_ecadc984._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_components_Chart_jsx_8a102011._.js",
-  "static/chunks/_31728dca._.js"
-],
-    source: "dynamic"
-});
